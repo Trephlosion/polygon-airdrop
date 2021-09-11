0e4174b55ae51547108e28c758556f351b1ed44b
beefJenson <trevorjohnson00@gmail.com>
2021-09-10 21:33:07 -0500

    9/10

--- contracts/airdrop.sol
+++ contracts/airdrop.sol
@@ -6,22 +6,31 @@
 
 pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
 import '@openzeppelin/contracts/token/ERC1155/utils/ERC1155Holder.sol';
+import "@openzeppelin/contracts/token/ERC1155/IERC1155Receiver.sol";
 import '@openzeppelin/contracts/access/Ownable.sol';
+import '@openzeppelin/contracts/utils/Context.sol';
 
 contract airDropper is Ownable, ERC1155Holder {
 
-    address tokenContractAddress = tokenContractAddress;
+    address tokenContractAddress;
     uint16 tokenId;
     IERC1155 currentToken;
 
+    function receiveLambs(uint _amount) public {
+        //currentToken.isApprovedForAll(address(this), msg.sender);
+        //currentToken.setApprovalForAll(msg.sender, true);
+        currentToken.safeTransferFrom(_msgSender(), address(this), 0, _amount, "");
+    }
+    
     function setTokenData(address _contractAddress, uint16 _tokenId) public onlyOwner() {
         tokenContractAddress = _contractAddress;
         tokenId = _tokenId;
         currentToken = IERC1155(tokenContractAddress);
     }
-
+    
     function drop(address[] memory dropAddresses) external onlyOwner() {
     // limit drops to 100
     require(dropAddresses.length < 101, "Dropping to too many addresses");
--- contracts/erc1155.sol
+++ contracts/erc1155.sol
@@ -7,11 +7,12 @@ import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
 import "@openzeppelin/contracts/token/ERC1155/IERC1155Receiver.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract RocketLamb is ERC1155, Ownable, IERC1155Receiver {
+contract RocketLamb is ERC1155, Ownable{
 
     string contractURI;
     IERC1155 currentToken;
-
+    uint currentSupply;
+    
     constructor() ERC1155(contractURI) {}
 
     function setContractURI(string memory _contractURI) public onlyOwner {
@@ -19,24 +20,13 @@ contract RocketLamb is ERC1155, Ownable, IERC1155Receiver {
     }
 
     function prepLambs(uint _amount) public onlyOwner {
-        _mint(address(this), 0, _amount, "");
+        require(currentSupply + _amount < 8501, "Too many lambs");
+        _mint(msg.sender, 0, _amount, "");
+        currentSupply = currentSupply + _amount;
     }
 
-    function showPreppedLambs() public view onlyOwner returns(uint) {
-        uint lambsPrepped = balanceOf(address(this), 0);
+    function showPreppedLambs(address _address) public view onlyOwner returns(uint) {
+        uint lambsPrepped = balanceOf(address(_address), 0);
         return lambsPrepped;
     }
 }
-    /* function setTokenData(address _contractAddress) public onlyOwner() {
-        currentToken = IERC1155(_contractAddress);
-    }
-
-    function launchLambs(address[] memory dropAddresses) external onlyOwner() {
-    // limit drops to 100
-        require(dropAddresses.length < 101, "Not enough rocketships");
-    
-            for(uint256 i; i < dropAddresses.length; i++){
-            address to = dropAddresses[i];
-            currentToken.safeTransferFrom(address(this), to, tokenId, 1, "");
-        }
-    } */
