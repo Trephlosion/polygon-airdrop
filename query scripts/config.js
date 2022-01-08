500a32a2dc2497da11ee7be8d10eca6b456322c4
beefJenson <trevorjohnson00@gmail.com>
2022-01-08 14:30:28 -0600

    Update config.js

--- query scripts/config.js
+++ query scripts/config.js
@@ -1,8 +1,6 @@
 // GET YOUR INFURA API ENDPOINT FROM https://infura.io/
 export const INFURA_ADDRESS = "<insert your API endpoint here>"
 
-
 export const ADDRESS = "contract address here"
-
 
 export const ABI = ["<insert ABI here>"]
